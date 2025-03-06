90d2a4ccdf0e2d876ddc65a5f1d403fe3134258d
Robert <vx16192@outlook.com>
2025-03-06 16:40:48 +0800

    (add)[yjnb]第一次提交

---
+++ yjnb-admin/src/main/java/com/yjnb/web/controller/monitor/ServerController.java
@@ -0,0 +1,27 @@
+package com.yjnb.web.controller.monitor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.yjnb.common.core.domain.AjaxResult;
+import com.yjnb.framework.web.domain.Server;
+
+/**
+ * 服务器监控
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/monitor/server")
+public class ServerController
+{
+    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
